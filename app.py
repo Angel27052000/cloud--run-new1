bc9d369f31dec6115502a79baa6b23b6965b8391
27angelrani@gmail.com <27angelrani@gmail.com>
2023-10-27 18:04:28 +0530

    updated the host

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def user_details():
     return render_template("user_details.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8080,host="localhost")
+    app.run(debug=True,port=8080,host="0.0.0.0")
 
